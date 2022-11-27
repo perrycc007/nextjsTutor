f62a872942ae45dbc5293812f8f84c256d30e52f
perrycc007 <77976566+perrycc007@users.noreply.github.com>
2022-11-27 22:59:56 +0000

    many bug fix profile and student apply

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,65 +1,58 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-
-router.get("/:userid", async(req, res) => {
-    const userid = parseInt(req.params.userid);
-    const result = await prisma.student.findUnique({
+router.get("/:userid", async (req, res) => {
+  const userid = parseInt(req.params.userid);
+  const result = await prisma.student.findUnique({
     where: {
-      userid: userid ,
+      userid: userid,
     },
-  })  
-    if (result);
-        console.log(result)
-        res.json({ result})
-  })
-  
-  router.patch("/", async(req, res) => {
-    const information = req.body.information
-    const studentid = parseInt(req.body.studentid);
-    const result = await prisma.student.update({
-        where: {
-          studentid: studentid
-        },
-        data: {
-          ...information
-        },
-      }
-    )
-    const createMatch = await prisma.match.upsert({
-      where: {
-        studentid: studentid
-      },
-      update: {
-      },
-      create: {
-        studentid:studentid,
-      },
-    }
-  )
-    console.log(result,createMatch)
-    res.json({result})
-    });
-    
-    router.post("/", async(req, res) => {
-      const information = req.body.information
-      const userid = parseInt(req.body.userid);
-      const result = await prisma.student.create({
-          data: {
-            userid:userid,
-            ...information
-          },
-        }
-      )
-      console.log(result)
-      res.json({result})
-      });
-   
- 
+  });
+  if (result);
+  console.log(result);
+  res.json({ result });
+});
+
+router.patch("/", async (req, res) => {
+  const information = req.body.information;
+  const studentid = parseInt(req.body.studentid);
+  const result = await prisma.student.update({
+    where: {
+      studentid: studentid,
+    },
+    data: {
+      ...information,
+    },
+  });
+  const createMatch = await prisma.match.upsert({
+    where: {
+      studentid: studentid,
+    },
+    update: {},
+    create: {
+      studentid: studentid,
+    },
+  });
+  console.log(result, createMatch);
+  res.json({ result });
+});
+
+router.post("/", async (req, res) => {
+  const information = req.body.information;
+  const userid = parseInt(req.body.userid);
+  const result = await prisma.student.create({
+    data: {
+      userid: userid,
+      ...information,
+    },
+  });
+  console.log(result);
+  res.json({ result });
+});
 
 module.exports = router;
