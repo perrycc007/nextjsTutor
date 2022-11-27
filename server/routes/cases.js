6c6b851da37e099453b3aaf41d26966a3ccd4034
perrycc007 <77976566+perrycc007@users.noreply.github.com>
2022-11-27 20:17:23 +0000

    favouriteList update

--- server/routes/cases.js
+++ server/routes/cases.js
@@ -1,80 +1,98 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-
-router.get("/", async(req, res) => {
-const result = await prisma.student.findMany()
-if (result.status == 200);
-try {
-  res.json({result})
-} catch (error) {
-  res.json({error})
-}
-
-});
-  
-
-router.get("/:casesid", async(req, res) => {
-  console.log('rec')
-  const casesid = JSON.parse(req.params.casesid)
-  const result = await prisma.student.findUnique({
-    where:{
-      studentid: casesid
-    }
-  })
-  res.json(result)
-
-});
-
-
-router.post("/", async(req, res) => {
-  // console.log(req.body.preference)
-  const {fee,location,highestteachinglevel,subject} = req.body.preference
-  const preference = { 
-    highestteachinglevel:highestteachinglevel,
-    lowestfee:{
-        gte: fee[0],
-      }
+router.get("/", async (req, res) => {
+  const result = await prisma.student.findMany();
+  if (result.status == 200);
+  try {
+    res.json({ result });
+  } catch (error) {
+    res.json({ error });
   }
- if (fee[0] == null){
-  delete preference['lowestfee','highestfee']
- }
- if (highestteachinglevel[0] ==null){
-  delete preference['highestteachinglevel']
- }
-
-  const result = await prisma.tutor.findMany(
-    {where: 
-      preference
+});
+router.post("/getFavouriteCase/:userid", async (req, res) => {
+  const userid = parseInt(req.params.userid);
+  const favourite = await prisma.user.findUnique({
+    where: {
+      userid: userid,
     },
-  ) 
-  
-let found = location[0] != null? result.map((key)=>{if(JSON.parse(key.location).some((item)=> location.indexOf(item) >= 0))
-                                  {return (key)}}) : result
-console.log(found)
-found = found.filter((item)=> item != null)
-let found1 =  subject[0] != null ? found.map((key)=>{if(JSON.parse(key.subject).some((item)=> subject.indexOf(item) >= 0))
-                                  {return (key)}}): found
+  });
+  const caseidList = favourite ? favourite.favouritecaseid : [];
+  const result = await prisma.tutor.findMany({
+    where: {
+      studentid: { in: caseidList },
+    },
+  });
+  res.json({ result });
+});
 
+router.get("/:casesid", async (req, res) => {
+  console.log("rec");
+  const casesid = JSON.parse(req.params.casesid);
+  const result = await prisma.student.findUnique({
+    where: {
+      studentid: casesid,
+    },
+  });
+  res.json(result);
+});
 
+router.post("/", async (req, res) => {
+  // console.log(req.body.preference)
+  const { fee, location, highestteachinglevel, subject } = req.body.preference;
+  const preference = {
+    highestteachinglevel: highestteachinglevel,
+    lowestfee: {
+      gte: fee[0],
+    },
+  };
+  if (fee[0] == null) {
+    delete preference[("lowestfee", "highestfee")];
+  }
+  if (highestteachinglevel[0] == null) {
+    delete preference["highestteachinglevel"];
+  }
+
+  const result = await prisma.tutor.findMany({ where: preference });
+
+  let found =
+    location[0] != null
+      ? result.map((key) => {
+          if (
+            JSON.parse(key.location).some((item) => location.indexOf(item) >= 0)
+          ) {
+            return key;
+          }
+        })
+      : result;
+  console.log(found);
+  found = found.filter((item) => item != null);
+  let found1 =
+    subject[0] != null
+      ? found.map((key) => {
+          if (
+            JSON.parse(key.subject).some((item) => subject.indexOf(item) >= 0)
+          ) {
+            return key;
+          }
+        })
+      : found;
 
   try {
     // const result = JSON.parse(...s);
-  // console.log(found1)
-  found1 = found1.filter((item)=> item != null)
-  res.json(found1)
+    // console.log(found1)
+    found1 = found1.filter((item) => item != null);
+    res.json(found1);
   } catch (err) {
     // 👇️ This runs
-    console.log('Error: ', err.message);
-    res.json( err.message)
+    console.log("Error: ", err.message);
+    res.json(err.message);
   }
-
-
 });
 
 module.exports = router;
--- server/routes/tutor.js
+++ server/routes/tutor.js
@@ -1,134 +1,134 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken')
-const dummyTutor = require('../DUMMY/dummyTutor')
+const jwt = require("jsonwebtoken");
+const dummyTutor = require("../DUMMY/dummyTutor");
 
-
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // GET /tasks?completed=true
 // GET /tasks?limit=10&skip=20
 // GET /tasks?sortBy=createdAt:desc
-router.get("/", async(req, res) => {
-  const result = await prisma.tutor.findMany({
-  })
+router.get("/", async (req, res) => {
+  const result = await prisma.tutor.findMany({});
   if (result.status == 200);
-try {
-  res.json({result})
-} catch (error) {
-  res.json({error})
-}
-
+  try {
+    res.json({ result });
+  } catch (error) {
+    res.json({ error });
+  }
 });
-  
 
-router.post("/getFavouriteCase", async(req, res) => {
-  // console.log(req)    
-  tutoridList = req.body.tutoridList
-  // console.log(tutoridList)
+router.post("/getFavouriteCase/:userid", async (req, res) => {
+  const userid = parseInt(req.params.userid);
+  const favourite = await prisma.user.findUnique({
+    where: {
+      userid: userid,
+    },
+  });
+  const tutoridList = favourite ? favourite.favouritetutorid : [];
   const result = await prisma.tutor.findMany({
-        where: {
-            tutorid: { in: tutoridList },
-        }
-
-  })
-  res.json({result})
+    where: {
+      tutorid: { in: tutoridList },
+    },
+  });
+  res.json({ result });
 });
 
-
-router.get("/:userid", async(req, res) => {
+router.get("/:userid", async (req, res) => {
   const userid = parseInt(req.params.userid);
   const result = await prisma.tutor.findUnique({
-  where: {
-    tutorid: userid ,
-  },
-})  
-// console.log('tutor',result)
-    if (result !== null){
-      // console.log(result)
-      res.json({result})}
-    else{
-      const result = {userid: userid,...dummyTutor}
-      // console.log(result)
-      res.json({result})
-}
-})
-
-
-router.post("/", async(req, res) => {
-  // console.log(req.body.preference)
-  const {fee,location,highestteachinglevel,subject} = req.body.preference
-  const preference = { 
-    highestteachinglevel:highestteachinglevel,
-    lowestfee:{
-        gte: fee[0],
-      }
-  }
- if (fee[0] == null){
-  delete preference['lowestfee','highestfee']
- }
- if (highestteachinglevel[0] ==null){
-  delete preference['highestteachinglevel']
- }
-
-  const result = await prisma.tutor.findMany(
-    {where: 
-      preference
+    where: {
+      tutorid: userid,
     },
-  ) 
-  
-let found = location[0] != null? result.map((key)=>{if(JSON.parse(key.location).some((item)=> location.indexOf(item) >= 0))
-                                  {return (key)}}) : result
-console.log(found)
-found = found.filter((item)=> item != null)
-let found1 =  subject[0] != null ? found.map((key)=>{if(JSON.parse(key.subject).some((item)=> subject.indexOf(item) >= 0))
-                                  {return (key)}}): found
+  });
+  // console.log('tutor',result)
+  if (result !== null) {
+    // console.log(result)
+    res.json({ result });
+  } else {
+    const result = { userid: userid, ...dummyTutor };
+    // console.log(result)
+    res.json({ result });
+  }
+});
 
+router.post("/", async (req, res) => {
+  // console.log(req.body.preference)
+  const { fee, location, highestteachinglevel, subject } = req.body.preference;
+  const preference = {
+    highestteachinglevel: highestteachinglevel,
+    lowestfee: {
+      gte: fee[0],
+    },
+  };
+  if (fee[0] == null) {
+    delete preference[("lowestfee", "highestfee")];
+  }
+  if (highestteachinglevel[0] == null) {
+    delete preference["highestteachinglevel"];
+  }
 
+  const result = await prisma.tutor.findMany({ where: preference });
+
+  let found =
+    location[0] != null
+      ? result.map((key) => {
+          if (
+            JSON.parse(key.location).some((item) => location.indexOf(item) >= 0)
+          ) {
+            return key;
+          }
+        })
+      : result;
+  console.log(found);
+  found = found.filter((item) => item != null);
+  let found1 =
+    subject[0] != null
+      ? found.map((key) => {
+          if (
+            JSON.parse(key.subject).some((item) => subject.indexOf(item) >= 0)
+          ) {
+            return key;
+          }
+        })
+      : found;
 
   try {
     // const result = JSON.parse(...s);
-  // console.log(found1)
-  found1 = found1.filter((item)=> item != null)
-  res.json(found1)
+    // console.log(found1)
+    found1 = found1.filter((item) => item != null);
+    res.json(found1);
   } catch (err) {
     // 👇️ This runs
-    console.log('Error: ', err.message);
-    res.json( err.message)
+    console.log("Error: ", err.message);
+    res.json(err.message);
   }
-
-
 });
-  
 
-router.patch("/", async(req, res) => {
-  const information = req.body.information
-  if (information.subject == undefined){
-    information.subject = JSON.stringify([])
+router.patch("/", async (req, res) => {
+  const information = req.body.information;
+  if (information.subject == undefined) {
+    information.subject = JSON.stringify([]);
   }
   // console.log(req.body.information)
   const userid = parseInt(req.body.userid);
   // console.log(information)
   const result = await prisma.tutor.upsert({
-      where: {
-        tutorid: userid
-      },
-      update: {
-        ...information
-      },
-      create: {
-        userid:userid,
-        ...information
-      },
-    }
-  
-  )
-  // console.log(result)
-  res.json({result})
+    where: {
+      tutorid: userid,
+    },
+    update: {
+      ...information,
+    },
+    create: {
+      userid: userid,
+      ...information,
+    },
   });
-
-
+  // console.log(result)
+  res.json({ result });
+});
 
 module.exports = router;
