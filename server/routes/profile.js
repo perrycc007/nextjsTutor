d6e5bc80268c69d44de14e6c0b48d6a0eb78b09d
perrycc007 <77976566+perrycc007@users.noreply.github.com>
2022-12-03 15:14:11 +0000

    gt profile button added

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,53 +1,51 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken')
-const dummyProfile = require('../DUMMY/dummyProfile')
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
+const jwt = require("jsonwebtoken");
+const dummyProfile = require("../DUMMY/dummyProfile");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-
-router.get("/:userid", async(req, res) => {
-    const userid = parseInt(req.params.userid);
-    const result = await prisma.profile.findUnique({
+router.get("/:userid", async (req, res) => {
+  const userid = parseInt(req.params.userid);
+  const result = await prisma.profile.findUnique({
     where: {
-      userid: userid ,
+      userid: userid,
     },
-  })  
-  console.log('profile',result)
-    if (result !== null){
-        console.log(result)
-        res.json({result})}
-    else{
-        const result = {userid: userid,...dummyProfile}
-        console.log(result)
-        res.json({result})
-    }
-  })
-  
-  router.post("/", async(req, res) => {
-    const information = req.body.information
-    agreewith=information.agreewith
-    console.log(agreewith)
-    const userid = parseInt(req.body.userid);
-    const result = await prisma.profile.upsert({
-        where: {
-          userid: userid,
-        },
-        update: {
-          ...information,
-          agreewith: `${agreewith}`
-        },
-        create: {
-          userid:userid,
-          ...information,
-          agreewith: `${agreewith}`
-        },
-      
-    }
-    )
-    console.log(result)
-    res.json({result})
-    });
+  });
+  console.log("profile", result);
+  if (result !== null) {
+    console.log(result);
+    res.json({ result });
+  } else {
+    const result = { userid: userid, ...dummyProfile };
+    console.log(result);
+    res.json({ result });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const reqUserid = parseInt(req.body.userid);
+  let { userid, idprofile, ...information } = req.body.information;
+  agreewith = information.agreewith;
+  console.log(reqUserid);
+  // userid = parseInt(req.body.userid);
+  const result = await prisma.profile.upsert({
+    where: {
+      userid: reqUserid,
+    },
+    update: {
+      ...information,
+      // agreewith: `${agreewith}`,
+    },
+    create: {
+      userid: reqUserid,
+      ...information,
+      agreewith: `${agreewith}`,
+    },
+  });
+  console.log(result);
+  res.json({ result });
+});
 
 module.exports = router;
